aaad7f31f8bf905482e286d749308b3802da5cac
este.mendxz@gmail.com <estebanmendezm1995@gmail.com>
2024-02-14 19:33:48 -0500

    main RFC

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import {useSEO}  from './Hooks/useCEO';
 import Item from './components/Item';
 
-
 function App() {  
-
-  useSEO({ title: `(${Item.length}) Lista de tareas`, description: 'Añadir, editar, eliminar elementos' });
-
   return (
    <Item />
   )
--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,13 @@ const theme = extendTheme({
   config: {
     initialColorMode: 'dark'
   },
+  /*colors: {
+    brand: {
+      100: "#f7fafc",
+      
+      900: "#1a202c",
+    },
+  },*/
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
